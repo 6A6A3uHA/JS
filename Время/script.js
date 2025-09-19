ab86d7439f485dcad788efe636cbf413bbf6f49f
6A6A3uHA <markisa130106@gmail.com>
2025-09-19 12:41:49 +0300

    пробуем двигать круг

---
+++ Время/script.js
@@ -0,0 +1,15 @@
+console.log('Привет');
+setTimeout(() => {
+    console.log('Пока');
+    }, 5000);
+console.log('Как дела');
+setTimeout(() => {
+    
+}, timeout);
+let interval = setInterval(() => {
+   console.log('');
+    
+}, );
+setTimeout(() => {
+    clearInterval(interval)
+}, 10000);
