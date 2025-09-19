7d23efcd94a72c77252228a67a2766db7a666091
6A6A3uHA <markisa130106@gmail.com>
2025-09-19 12:41:58 +0300

    все еще

--- Время/script.js
+++ Время/script.js
@@ -1,15 +1,26 @@
-console.log('Привет');
-setTimeout(() => {
-    console.log('Пока');
-    }, 5000);
-console.log('Как дела');
-setTimeout(() => {
+// console.log('Привет');
+// setTimeout(() => {
+//     console.log('Пока');
+//     }, 5000);
+
+// console.log('Как дела');
+// setTimeout(() => {
+//     console.log('a');
     
-}, timeout);
-let interval = setInterval(() => {
-   console.log('');
+// }, 5);
+
+// let interval = setInterval(() => {
+//    console.log('');
     
-}, );
+// }, 1);
+// setTimeout(() => {
+//     clearInterval(interval)
+// }, 10000);
+// setInterval(() => {
+//     console.log('пока');
+    
+// }, 1);
+const circle = document.getElementById('circle')
 setTimeout(() => {
-    clearInterval(interval)
-}, 10000);
+    circle.classList.add('moveright')
+}, 3000);
