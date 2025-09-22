153b73308896be44a38f62e1fe034b96c4f44b8e
6A6A3uHA <markisa130106@gmail.com>
2025-09-22 12:49:10 +0300

    цвет меняется

--- Цвет/script.js
+++ Цвет/script.js
@@ -1,8 +1,12 @@
 const btn = document.getElementById('btn')
+let a = 0
 function changecolor() {
-    document.documentElement.style.setProperty('--color','green')
+    // document.documentElement.style.setProperty('--color','green')
+    a++
+    a%=raduga.length
+    document.documentElement.style.setProperty('--color', raduga[a])
 }
 
 btn.addEventListener('click', changecolor)
 
-let raduga=[]
+let raduga=['red','orange','yellow','green','blue','darkblue','purple']
