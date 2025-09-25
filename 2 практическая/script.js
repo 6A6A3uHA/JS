7df313f501e608632268a5db3c1e04c47e622b71
6A6A3uHA <markisa130106@gmail.com>
2025-09-25 09:17:46 +0300

    закончил с цветом, начало 2 практической с исходниками цвета

--- 2 практическая/script.js
+++ 2 практическая/script.js
@@ -1,16 +1,3 @@
-// const btn = document.getElementById('btn')
-// let a = 0
-// function changecolor() {
-//     // document.documentElement.style.setProperty('--color','green')
-//     a++
-//     a%=raduga.length
-//     document.documentElement.style.setProperty('--color', raduga[a])
-// }
-
-// btn.addEventListener('click', changecolor)
-
-// let raduga=['red','orange','yellow','green','blue','darkblue','purple']
-
 const btnl = document.getElementById('btnleft')
 const btnr = document.getElementById('btnright')
 let a = 0
