75c88ac834d6264b909693ff1127e445ce915876
6A6A3uHA <markisa130106@gmail.com>
2025-09-26 12:07:13 +0300

    3 пара

---
+++ 3 пара/script.js
@@ -0,0 +1,11 @@
+let inp1 = document.getElementById("inp1")
+let inp2 = document.getElementById("inp2")
+let btn = document.getElementById("btn")
+let res = document.getElementById("res")
+
+function sum() {
+    if (isNaN(inp1) || isNaN(inp2))
+    {
+        res.
+    }
+}
