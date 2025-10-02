6982c56441473f92954d6611693b93276fd2184a
6A6A3uHA <markisa130106@gmail.com>
2025-10-02 12:52:18 +0300

    Страница здоровается с записанным лицом

--- локальные данные/script.js
+++ локальные данные/script.js
@@ -1,1 +1,46 @@
-localStorage.setItem('name', 'Misha')
+// localStorage.setItem('name', 'Misha')
+// a=localStorage.getItem('name')
+// console.log(a);
+// localStorage.removeItem('name')
+// localStorage.clear()
+
+const btns = document.getElementById('save')
+const btne = document.getElementById('erase')
+const inp = document.getElementById('inp')
+const res = document.getElementById('res')
+let a = localStorage.getItem('name')
+
+function save()
+{
+    localStorage.setItem('name', inp.value)
+    if (inp.value == null|| inp.value.trim() == '')
+{
+    res.innerText = 'Я тебя не знаю'
+    inp.value=''
+}
+else
+{
+    res.innerText = 'С возвращением '+inp.value
+    inp.value=''
+}
+}
+
+function erase()
+{
+    localStorage.clear()
+    res.innerText = 'Я тебя не знаю'
+    inp.value=''
+}
+
+if (a == null|| a.trim() == '')
+{
+    res.innerText = 'Я тебя не знаю'
+}
+
+else
+{
+    res.innerText = 'С возвращением '+a
+}
+
+btns.addEventListener('click', save)
+btne.addEventListener('click', erase)
