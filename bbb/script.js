e07e8034cbe1b14ca400f429f7d13d99530e7ae6
6A6A3uHA <markisa130106@gmail.com>
2025-10-09 13:38:19 +0300

    переключаем отображаемые дивы по кнопке

--- bbb/script.js
+++ bbb/script.js
@@ -1,24 +1,45 @@
-const polygon = document.getElementById('polygon')
-const inp = document.getElementById('inp')
-const btn = document.getElementById('btn')
+// const polygon = document.getElementById('polygon')
+// const inp = document.getElementById('inp')
+// const btn = document.getElementById('btn')
 
-function create() {
-    const block = document.createElement('li')
-    const elem = document.createElement('p')
-    const button = document.createElement('button')
-    elem.textContent = inp.value
-    button.textContent = 'Ухади'
-    block.classList.add('line')
-    polygon.appendChild(block)
-    block.appendChild(elem)
-    block.appendChild(button)
+// function create() {
+//     const block = document.createElement('li')
+//     const elem = document.createElement('p')
+//     const button = document.createElement('button')
+//     elem.textContent = inp.value
+//     button.textContent = 'Ухади'
+//     block.classList.add('line')
+//     polygon.appendChild(block)
+//     block.appendChild(elem)
+//     block.appendChild(button)
 
-    function del(params) {
-        button.removeEventListener('click', del)
-        block.remove()
-    }
-    button.addEventListener('click', del)
+//     function del(params) {
+//         button.removeEventListener('click', del)
+//         block.remove()
+//     }
+//     button.addEventListener('click', del)
+// }
+
+
+// btn.addEventListener('click', create)
+
+const btn1 = document.getElementById('1')
+const btn2 = document.getElementById('2')
+const btn3 = document.getElementById('3')
+const listclass = document.getElementsByClassName('hide')
+let active = 0
+
+console.log(listclass);
+
+
+function change(newactive){
+    console.log(active, newactive);
+    
+    listclass[active].classList.toggle('look')
+    listclass[newactive].classList.toggle('look')
+    active = newactive
 }
 
-
-btn.addEventListener('click', create)
+// btn1.addEventListener('click', () => change(btn1.value))
+// btn2.addEventListener('click', () => change(btn2.value))
+// btn3.addEventListener('click', () => change(btn3.value))
